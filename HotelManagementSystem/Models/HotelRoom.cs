b87aa0a03d647adc22db16f6d31f116cc786b6da
Christopher Averkos <choclate00@live.com>
2023-10-13 21:17:24 +1100

    Created hotelRoom class

---
+++ HotelManagementSystem/Models/HotelRoom.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem.Models
+{
+    enum RoomClass
+    {
+        Economy,
+        Standard,
+        Platnum,
+        Premium
+    }
+    internal class HotelRoom
+    {
+        private int roomCode;
+        private string roomName;
+        private RoomClass roomClass;
+        private bool isAvailable;
+        private double pricePerNight;
+        private double discountedPrice;
+        private int levelNumber; //this is used to determine what level of the hotel building the room is on.
+        private double roomLength;
+        private double roomHeight;
+        private int maximumGuests;
+        private bool petsAllowed;
+
+        public double RoomSize 
+        {
+            get => roomLength * roomHeight; 
+        }
+
+        public double PricePerHour
+        {
+            get => pricePerNight / 24;
+        }
+
+        public bool IsDiscount
+        {
+            get => discountedPrice < pricePerNight;
+        }
+
+    }
+}
