a45e852fc4caa24f12c96def01a3032ac895ebfe
Christopher Averkos <choclate00@live.com>
2023-10-22 15:54:25 +1100

    Not yet tested, implemented the book room stuff without validation.

--- assignment2v6.0/WinFormsApp1/BookRoomForm.cs
+++ assignment2v6.0/WinFormsApp1/BookRoomForm.cs
@@ -12,11 +12,14 @@ namespace assignment2
 {
     public partial class BookRoomForm : Form
     {
-        private int customerId;
+        private string customerUserName;
         private int roomId;
-        public BookRoomForm()
+        public BookRoomForm(string customerUserName, int roomId)
         {
             InitializeComponent();
+            this.customerUserName = customerUserName;
+            this.roomId = roomId;
+
         }
 
         private void numericUpDown2_ValueChanged(object sender, EventArgs e)
@@ -30,8 +33,27 @@ namespace assignment2
         }
 
         private void btnBook_Click(object sender, EventArgs e)
-        {
+        {   
+            //validation to check if the room is vacant will come later.
+            //generates random id
+            Random random = new Random();
+            int randomId = random.Next();
+                
+            DateTime checkInDate = dtpCheckInDate.Value;
+            DateTime checkOutDate = dtpCheckOutDate.Value;
+            int numbersOfGuests = Convert.ToInt32(numDpGuests.Value);
+            int numbersOfDependents = Convert.ToInt32(numDpDependents.Value);
+            RoomBooking newBooking = new RoomBooking(randomId, customerUserName, roomId, checkInDate, checkOutDate, numbersOfGuests, numbersOfDependents);
+            //stores it into a text file
+            AddToTxtFile(newBooking);
+            MessageBox.Show("Room has been booked sucecsessfuly.");
+        }
 
+        // stores booking details into a text file.
+        public void AddToTxtFile(RoomBooking roomBooking)
+        {
+            //appends it to a text file
+            File.AppendAllText("roomBookings.txt", roomBooking.GetTxtFileString());
         }
     }
 }
--- assignment2v6.0/WinFormsApp1/RoomBooking.cs
+++ assignment2v6.0/WinFormsApp1/RoomBooking.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HotelManagementSystem.Models
+namespace assignment2
 {
     public class RoomBooking
     {
