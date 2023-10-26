f7909dd392e16e499400b992569eafe2fa22b899
Christopher Averkos <choclate00@live.com>
2023-10-26 11:32:23 +1100

    Rooms are showing on Receptionist Form.

--- assignment2v6.0/WinFormsApp1/ReceptionistForm.cs
+++ assignment2v6.0/WinFormsApp1/ReceptionistForm.cs
@@ -47,6 +47,7 @@ namespace assignment2
         {
             //loads the customer data from memory to the table
             AddCustomersToDataTable();
+            AddRoomsToDT();
         }
 
         private void btnBook_Click(object sender, EventArgs e)
@@ -61,16 +62,43 @@ namespace assignment2
             DataColumn roomDisplayTextCol = new DataColumn();
             roomDisplayTextCol.ColumnName = "name";
             roomsDataTable.Columns.Add(roomCodeCol);
-            roomsDataTable.Columns.Add( roomDisplayTextCol);
+            roomsDataTable.Columns.Add(roomDisplayTextCol);
 
+        }
+
+        //this is a uhelper function that will add the data from the text file to the room datatable.
+        private void FetchRoomsFromTxt()
+        {
+            foreach(string line in File.ReadAllLines("rooms.txt"))
+            {
+                string[] splits = line.Split(',');
+                var row = roomsDataTable.NewRow();
+                row["code"] = splits[0];
+                row["name"] = "Room: #" + splits[0]; 
+                roomsDataTable.Rows.Add(row);
+            }
+        }
+
+        private void RefreshRoomsDT()
+        {
+            lbRooms.DataSource = roomsDataTable;
+            lbRooms.DisplayMember = "name";
+            lbRooms.ValueMember = "code";
         }
 
         private void AddCustomersToDataTable()
         {
-            AddColumns();
+            AddCustColumns();
             AddCustomerDataFromDB();
             RefreshCustomerList();
 
+        }
+
+        private void AddRoomsToDT()
+        {
+            AddRoomCols();
+            FetchRoomsFromTxt();
+            RefreshRoomsDT();
         }
 
         private void RefreshCustomerList()
@@ -79,7 +107,7 @@ namespace assignment2
             lbCustomers.DisplayMember = "name";
             lbCustomers.ValueMember = "username";
         }
-        private void AddColumns()
+        private void AddCustColumns()
         {
             DataColumn custUserNameCol = new DataColumn();
             custUserNameCol.ColumnName = "username";
