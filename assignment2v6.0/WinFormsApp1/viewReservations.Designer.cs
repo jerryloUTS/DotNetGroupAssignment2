72c7802a36ae24224446353a187d61783ed0b1e9
smollyf <saoban.salwa@yahoo.com>
2023-10-29 00:56:13 +1100

    view reservations functionality added

--- assignment2v6.0/WinFormsApp1/viewReservations.Designer.cs
+++ assignment2v6.0/WinFormsApp1/viewReservations.Designer.cs
@@ -28,7 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             label1 = new Label();
+            dataGridView = new DataGridView();
+            closebtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -41,14 +46,58 @@
             label1.TabIndex = 0;
             label1.Text = "Guest Reservations for Dinner";
             // 
-            // Form2
+            // dataGridView
+            // 
+            dataGridView.AllowUserToOrderColumns = true;
+            dataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllHeaders;
+            dataGridView.BackgroundColor = SystemColors.ButtonFace;
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Window;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.False;
+            dataGridView.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView.Location = new Point(112, 127);
+            dataGridView.Name = "dataGridView";
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.Padding = new Padding(1);
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridView.RowHeadersWidth = 65;
+            dataGridView.RowTemplate.Height = 29;
+            dataGridView.Size = new Size(425, 275);
+            dataGridView.TabIndex = 1;
+            // 
+            // closebtn
+            // 
+            closebtn.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            closebtn.Location = new Point(278, 474);
+            closebtn.Name = "closebtn";
+            closebtn.Size = new Size(95, 37);
+            closebtn.TabIndex = 2;
+            closebtn.Text = "Close";
+            closebtn.UseVisualStyleBackColor = true;
+            closebtn.Click += closebtn_Click;
+            // 
+            // viewReservations
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(663, 557);
+            Controls.Add(closebtn);
+            Controls.Add(dataGridView);
             Controls.Add(label1);
-            Name = "Form2";
+            Name = "viewReservations";
             Text = "Dinner Reservations";
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -56,5 +105,7 @@
         #endregion
 
         private Label label1;
+        private DataGridView dataGridView;
+        private Button closebtn;
     }
 }
--- assignment2v6.0/WinFormsApp1/viewReservations.cs
+++ assignment2v6.0/WinFormsApp1/viewReservations.cs
@@ -15,6 +15,59 @@ namespace assignment2
         public viewReservations()
         {
             InitializeComponent();
+            InitializeDataGridView();
+        }
+
+        private void InitializeDataGridView()
+        {
+            // Set up the columns
+            dataGridView.Columns.Add("Room", "Room");
+            dataGridView.Columns.Add("Date & Time", "Date & Time");
+            //dataGridView.Columns.Add("Time", "Time");
+            dataGridView.Columns.Add("Guests", "Guests");
+
+            // Load and populate data from the DiningReservations text file
+            LoadReservationsFromFile();
+        }
+
+        private void LoadReservationsFromFile()
+        {
+            try
+            {
+                // Read all lines from the DiningReservations text file
+                string[] lines = File.ReadAllLines("DinnerReservations.txt");
+
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(',');
+
+                    // Check if the line has at least four parts
+                    if (parts.Length >= 4)
+                    {
+                        // Extract values
+                        string room = parts[0].Trim();
+                        //string date = parts[1].Trim();
+                        string dateTime = parts[2].Trim();
+                        string guests = parts[3].Trim();
+
+                        // Add the data to the DataGridView
+                        dataGridView.Rows.Add(room, dateTime, guests);
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("The file is not found.", "File Not Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void closebtn_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
