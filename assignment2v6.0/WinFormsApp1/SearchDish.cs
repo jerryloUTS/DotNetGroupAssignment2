6cad9d8521e6b089265f5142a9974d7891a478b4
mistahee <147353264+mistahee@users.noreply.github.com>
2023-10-29 13:56:09 +1100

    Update SearchDish.cs

--- assignment2v6.0/WinFormsApp1/SearchDish.cs
+++ assignment2v6.0/WinFormsApp1/SearchDish.cs
@@ -48,7 +48,12 @@ namespace assignment2
                     {
                         string[] dishSplit = line.Split(',');
                         string[] dishSplits = line.Split(',');
-                        richTextBox1.AppendText(dishSplit[0] + "\n");
+                        richTextBox1.AppendText("Dish:" + dishSplit[0] + "\n");
+                        richTextBox1.AppendText("Price: " + dishSplit[1] + "\n");
+                        richTextBox1.AppendText("Calories (kJ): " + dishSplit[2] + "\n");
+                        richTextBox1.AppendText("Meal Type: " + dishSplit[3] + "\n");
+                        richTextBox1.AppendText("Allergens: " + dishSplit[4] + "\n");
+                        richTextBox1.AppendText("Additional Information: " + dishSplit[5] + "\n");
                     }
                 }
                 
