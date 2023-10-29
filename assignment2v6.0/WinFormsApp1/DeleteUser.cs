c97040a953098bf459402e95b3ed43caf20eb5ae
mistahee <147353264+mistahee@users.noreply.github.com>
2023-10-29 15:34:22 +1100

    Update DeleteUser.cs

--- assignment2v6.0/WinFormsApp1/DeleteUser.cs
+++ assignment2v6.0/WinFormsApp1/DeleteUser.cs
@@ -18,7 +18,7 @@ namespace assignment2
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) //Closes DeleteUser
         {
             Close();
         }
