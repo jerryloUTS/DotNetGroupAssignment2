7c77eb24813610e2a0c0b058796b0f2deed4b019
mistahee <147353264+mistahee@users.noreply.github.com>
2023-10-29 14:10:35 +1100

    Update SearchDish.cs

--- assignment2v6.0/WinFormsApp1/SearchDish.cs
+++ assignment2v6.0/WinFormsApp1/SearchDish.cs
@@ -20,7 +20,7 @@ namespace assignment2
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) //Exit searchDish
         {
             Close();
         }
@@ -28,26 +28,25 @@ namespace assignment2
         private void button2_Click(object sender, EventArgs e)
         {
             var lines = File.ReadAllText("dish.txt");
-            if (!File.Exists(dishSearch.Text + ".txt"))
+            if (!File.Exists(dishSearch.Text + ".txt")) //Error if the file does not exist according to the user input
             {
                 MessageBox.Show(dishSearch.Text + " was not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
-            else if (File.Exists(dishSearch.Text + ".txt"))
+            else if (File.Exists(dishSearch.Text + ".txt")) //Displays the dish if the file exists according to the user input
             {
                 
                 if (!lines.Contains(dishSearch.Text, StringComparison.OrdinalIgnoreCase))
                 {
                     MessageBox.Show("Dish not found. Please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 }
-                if (lines.Contains(dishSearch.Text, StringComparison.OrdinalIgnoreCase))
+                if (lines.Contains(dishSearch.Text, StringComparison.OrdinalIgnoreCase)) //allows the user to search for the function without relying on case-sensitive characters
                 {
                     richTextBox1.Clear();
                     List<string> dishInfo = new List<string>();
                     dishInfo = File.ReadAllLines(dishSearch.Text + ".txt").ToList();
                     foreach (var line in dishInfo)
                     {
-                        string[] dishSplit = line.Split(',');
-                        string[] dishSplits = line.Split(',');
+                        string[] dishSplit = line.Split(','); ////Splits the line into an array of its own
                         richTextBox1.AppendText("Dish: " + dishSplit[0] + "\n");
                         richTextBox1.AppendText("Price: " + dishSplit[1] + "\n");
                         richTextBox1.AppendText("Calories (kJ): " + dishSplit[2] + "\n");
